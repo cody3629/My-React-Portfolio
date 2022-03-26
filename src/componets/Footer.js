a88abb6430293b4f953b36a0823282c07133accb
Cody Webb <webbcody36@gmail.com>
2022-03-26 14:16:41 -0400

    added navigation, page setup

---
+++ src/componets/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+          <strong>My Portfolio</strong>
+          .
+          <hr />
+          <a
+            href=""
+            target="_blank" rel="noreferrer"
+          >
+            LinkedIn
+          </a>{" "}
+          |{" "}{" "}
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
