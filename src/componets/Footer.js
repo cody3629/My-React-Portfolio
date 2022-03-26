5c3d54f583970c3e433e42bb745fdc1d06abf750
Cody Webb <webbcody36@gmail.com>
2022-03-26 15:58:40 -0400

    added resume functionality, added links, and adjusted about me page

--- src/componets/Footer.js
+++ src/componets/Footer.js
@@ -9,12 +9,19 @@ function Footer() {
           .
           <hr />
           <a
-            href=""
+            href="https://www.linkedin.com/in/cody-webb-6041a718b/"
             target="_blank" rel="noreferrer"
           >
             LinkedIn
           </a>{" "}
           |{" "}{" "}
+          <a
+            href="https://github.com/cody3629"
+            target="_blank" rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          {" "}{" "}
         </p>
       </div>
     </footer>
---
+++ src/componets/Resume.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Resume() {
+  return (
+    <div className="columns">
+      <div className="column">
+        <p className="content is-medium">Resume</p>
+        <hr />
+
+        <a
+          className="button is-primary"
+          href={"/img/myresume.pdf"}
+          target="_blank" rel="noreferrer"
+        >
+          <span className="icon">
+            <i className="fas fa-download"></i>
+          </span>
+          <span>Download My Resume</span>
+        </a>
+      </div>
+      <div className="column">
+        <p className="content is-medium">Skills</p>
+        <hr />
+        <ul>
+            <li>HTML, CSS, Javascript</li>
+            <li>MySQL, Express.js, JSON</li>
+            <li>React, Handlebars.js, Bootstrap</li>
+            <li>REST API's, AJAX, MongoDB</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Resume;
